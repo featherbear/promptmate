c22128cb9f61cb7b8b4ec765ab107b71103017bc
Andrew Wong <andrew.j.wong@outlook.com>
2021-05-04 19:30:39 +1000

    Add interface for main teleprompter interface

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,22 @@
 <script lang="ts">
-  export let name: string;
+  import PromptInterface from "./components/PromptInterface";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <PromptInterface />
 </main>
 
 <style lang="scss">
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-
-    h1 {
-      color: #ff3e00;
-      text-transform: uppercase;
-      font-size: 4em;
-      font-weight: 100;
-    }
+  :root {
+    @import "./styles/global.scss";
+    background-color: $bg;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  main {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
   }
 </style>
---
+++ src/components/PromptInterface/PromptInterface.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  let contentIsMirrored = false;
+  const toggleMirroredView = () => (contentIsMirrored = !contentIsMirrored);
+</script>
+
+<button on:click={toggleMirroredView} />
+<div class="interface">
+  <header>
+    <!-- Controls go here -->
+  </header>
+  <section class:mirrored={contentIsMirrored}>
+    <!-- Main teleprompter content go here -->
+  </section>
+  <footer>
+    <!-- Do we need a footer -->
+  </footer>
+</div>
+
+<style lang="scss">
+  .interface {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    section {
+      flex: 1;
+
+      &.mirrored {
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
+      }
+    }
+  }
+</style>
