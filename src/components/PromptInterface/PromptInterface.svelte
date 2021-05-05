b5c443e41ac796573fea88a265000bafd4836e8c
Andrew Wong <andrew.j.wong@outlook.com>
2021-05-05 17:42:38 +1000

    Add scrollable textbox

--- src/components/PromptInterface/PromptInterface.svelte
+++ src/components/PromptInterface/PromptInterface.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
   let contentIsMirrored = false;
   const toggleMirroredView = () => (contentIsMirrored = !contentIsMirrored);
+
+  import stub from "./stub";
 </script>
 
-<button on:click={toggleMirroredView} />
+<!-- <button on:click={toggleMirroredView} /> -->
 <div class="interface">
   <header>
     <!-- Controls go here -->
+    // controls here
   </header>
   <section class:mirrored={contentIsMirrored}>
+    <!-- TODO: Edit mode -->
+    <div class="content">
+      {stub}
+    </div>
     <!-- Main teleprompter content go here -->
   </section>
   <footer>
     <!-- Do we need a footer -->
+    some footer text // file name blah blah
   </footer>
 </div>
 
@@ -26,6 +34,23 @@
 
     section {
       flex: 1;
+
+      position: relative;
+
+      .content {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+
+        overflow-y: scroll;
+
+        scrollbar-width: none; /* Firefox */
+        -ms-overflow-style: none; /* Internet Explorer 10+ */
+
+        &:-webkit-scrollbar {
+          display: none;
+        }
+      }
 
       &.mirrored {
         -webkit-transform: scaleX(-1);
